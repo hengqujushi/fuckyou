6843dfd454d5cef12c7d1caca377285d3aefbcd4
zyy <zhang.huanzhiyuan@gmail.com>
2015-06-29 14:55:06 +0800

    Initial Commit

---
+++ testdemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testdemo
+//
+//  Created by li刚 on 15/6/29.
+//  Copyright (c) 2015年 zhangyuanyuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
